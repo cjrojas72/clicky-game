63f0aed62ee73330ceca776e86b804f920b8d1f8
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-03-05 20:00:44 -0500

    adding build

---
+++ build/precache-manifest.3b64246563e714af7503d7f9b2f4c8f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "27b97be10c7731e0dec9",
+    "url": "/clicky-game/static/js/main.27b97be1.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "b3235c4d861472168de79fd2226ff5ff",
+    "url": "/clicky-game/index.html"
+  }
+];
